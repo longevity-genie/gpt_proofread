926ea383f8289d9712796993e1869bc16e334692
Newton Winter <76408211+winternewt@users.noreply.github.com>
2023-05-23 11:33:07 +0300

    Tested on 256 papers
    Fixes in chunk calculation and error handling

--- ocr.py
+++ ocr.py
@@ -1,23 +1,38 @@
+import os
 import asyncio
 import textwrap
+import click
+import dotenv
+import openai
+import tiktoken
+
 from pathlib import Path
 from time import sleep
 from typing import List, Tuple, Dict
-
-import click
-import openai
-import tiktoken
 from click import Context
+from dotenv import load_dotenv
 
-openai.api_key = open('openaiapi.key', 'r', encoding='utf-8').read()
+env = dotenv.find_dotenv()
+#print(f"environment found at {env}")
+has_env: bool = load_dotenv(env, verbose=True)
+if not has_env:
+    print("Did not found environment file, using system OpenAI key (if exists)")
+    openai_key = os.getenv('OPENAI_API_KEY')
+if openai_key is None:
+    print("No key found, fallback to file:")
+    with open('openaiapi.key', 'r', encoding='utf-8') as f:
+        openai_key = f.read()
+#print(f"OPENAI key is {openai_key}")
+
+openai.api_key = openai_key
 default_model = "gpt-3.5-turbo"
 payload_wrap = '```<<PAYLOAD>>```'
 payload_keyword = '```<<PAYLOAD>>```'
 prompt_cap = 4096
 completion = 1000
-max_retry = 10
+max_retry = 5
 
-CHATCOMPLETION_OPTIONS = {
+CHAT_COMPLETION_OPTIONS = {
     "temperature": 0.6,
     "max_tokens": 1000,
     "top_p": 1,
@@ -27,12 +42,12 @@ CHATCOMPLETION_OPTIONS = {
 }
 
 
-def generate_prompt_messages(message: str, prompt: str, dialog_messages: List[Dict[str, str]]):
-    messages = [{"role": "system", "content": str(prompt)}]
+def generate_prompt_messages(message: str, prompt: str, dialog_messages: List[Dict[str, str]]) -> List[Dict[str, str]]:
+    messages = [{"role": "system", "content": prompt}]
     for dialog_message in dialog_messages:
-        messages.append({"role": "user", "content": str(dialog_message["user"])})
-        messages.append({"role": "assistant", "content": str(dialog_message["assistant"])})
-    messages.append({"role": "user", "content": str(message)})
+        messages.append({"role": "user", "content": dialog_message["user"]})
+        messages.append({"role": "assistant", "content": dialog_message["assistant"]})
+    messages.append({"role": "user", "content": message})
 
     return messages
 
@@ -53,17 +68,20 @@ async def prompt_ai(uid: str, prompt: str, payload: str, model: str, wrap: str) 
             r = await openai.ChatCompletion.acreate(
                 model=model,
                 messages=messages,
-                **CHATCOMPLETION_OPTIONS
+                **CHAT_COMPLETION_OPTIONS
             )
             answer = r.choices[0].message["content"]
             input_tokens, output_tokens = r.usage.prompt_tokens, r.usage.completion_tokens
         except openai.error.RateLimitError:
             sleep(5)
+            take += 1
+            if take >= max_retry:
+                raise e
         except openai.error.InvalidRequestError as e:  # too many tokens
             take += 1
             if take >= max_retry:
                 raise e
-    print(f"id={id}/finished on try {take + 1} of {max_retry}!")
+    print(f"id={uid}/finished on try {take + 1} of {max_retry}!")
     return answer, input_tokens, output_tokens
 
 
@@ -76,20 +94,23 @@ def num_tokens(string: str, model: str) -> int:
 
 async def prompt_in_chunks(text: str, prompt: str, model: str, wrap: str) -> Tuple[str, int]:
     c_limit = prompt_cap - completion - num_tokens(prompt, model)  # both input and completion must fit
+    body_size = num_tokens(text, model)
+    min_chunks = 1 + body_size // c_limit #size of entire body ceil divided by cap
     act_size = c_limit
-    size = c_limit
+    wrap_size = len(text) // min_chunks
     chunks = []
     while act_size >= c_limit:
-        size = size - 1 - (act_size - size) // 4
-        chunks = textwrap.wrap(text, size * 4)
+        chunks = textwrap.wrap(text, wrap_size) #cut
         act_size = 0
         for chunk in chunks:
             tokens = num_tokens(chunk, model)
-            if act_size < tokens:
-                act_size = tokens
-        # print(f"{size}/{act_size}/{c_limit} - expectation/real size/limit")
+            if act_size <= tokens:
+                act_size = tokens + 4 #error reserve
 
-    print(f"expectation={size}/tokens={act_size}/limit={c_limit}/total_chunks={len(chunks)}")
+        wrap_size = wrap_size - 1 - (act_size*4 - wrap_size) // 8 #min reduce by 1 to avoid inf cycle if floor div gives 0
+        #print(f"{wrap_size}/{act_size}/{c_limit} - expectation/real size/limit")
+
+    print(f"expectation={wrap_size}/tokens={act_size}/limit={c_limit}/total_chunks={len(chunks)}")
     tasks = [prompt_ai(f"ID_{i + 1}", prompt, chunk, model, wrap) for i, chunk in enumerate(chunks)]
     results = await asyncio.gather(*tasks)
     joined_results = ''.join(result[0] for result in results)
@@ -133,9 +154,7 @@ async def write_async(model: str, keyword: str, prompt_file: str, base: str):
 @click.group(invoke_without_command=False)
 @click.pass_context
 def app(ctx: Context):
-    # if ctx.invoked_subcommand is None:
-    #    click.echo('Running the default command...')
-    #    test()
+
     pass
 
 
